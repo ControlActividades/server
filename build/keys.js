48af1ef24464cdf4f7cd0b3f6d0d7bf833d561ca
IsaacCanoH <isac.c.2586@gamil.com>
2024-08-21 17:34:38 -0600

    Server2

--- build/keys.js
+++ build/keys.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
         host: 'db-mysql-do-user-17557286-0.f.db.ondigitalocean.com',
-        user: 'doadmin',
-        password: 'AVNS_FajkTY8h6zCnkWckIxK',
+        user: 'admin',
+        password: 'AVNS_OaRkBfJ6ZEBSq-UDoM2',
         database: 'controlActividadesUtng',
         port: 25060
     }
