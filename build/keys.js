7f0c7071b6ad9b0a988b8175133a693617335338
IsaacCanoH <isac.c.2586@gamil.com>
2024-08-21 17:13:46 -0600

    first commit

---
+++ build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'db-mysql-do-user-17557286-0.f.db.ondigitalocean.com',
+        user: 'doadmin',
+        password: 'AVNS_FajkTY8h6zCnkWckIxK',
+        database: 'controlActividadesUtng',
+        port: 25060
+    }
+};
